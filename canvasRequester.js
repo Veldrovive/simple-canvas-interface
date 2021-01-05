c19c16e5d01d75847e3e7f8d2a16fcd43d9cae8e
Veldrovive <aidan.dempster@gmail.com>
2021-01-05 17:16:06 -0500

    Added a check to make sure links exist

--- canvasRequester.js
+++ canvasRequester.js
@@ -80,7 +80,7 @@ class Requester {
                         let body = res.data
                         const linkHeader = res.headers.link
                         const parsedLinks = parseLinks(linkHeader)
-                        if ('next' in parsedLinks) {
+                        if (parseLinks != null && 'next' in parsedLinks) {
                             const nextPageEndpoint = parsedLinks['next'].url
                             // We recursively call this same function for getting more pages until the base condition of there being no next page
                             const followingPagesContents = await this.spawnRequest(nextPageEndpoint)
